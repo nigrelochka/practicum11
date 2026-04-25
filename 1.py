dd27af5e2d18d54dffd5553d34aeec32cef8f8c7
nigrelochka <94745274+nigrelochka@users.noreply.github.com>
2026-04-25 13:44:40 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,9 +1,9 @@
 list_of_10numbers = [int(x) for x in input('введите числа: ').split()]
 
-if len(list_of_10numbers)==10:
+if len(list_of_10numbers) == 10:
     list_of_8numbers = []
     for _ in range(9):
-        num = list_of_10numbers[_] + list_of_10numbers[_+1]
+        num = list_of_10numbers[_] + list_of_10numbers[_ + 1]
         list_of_8numbers.append(num)
     print(*list_of_8numbers)
 else:
