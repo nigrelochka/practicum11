8ece7457fe5d5afc242ab1f46022d3accae599e5
nigrelochka <94745274+nigrelochka@users.noreply.github.com>
2026-04-25 13:44:14 +0700

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,8 +1,8 @@
 list_of_numbers = [int(x) for x in input('введите числа: ').split()]
-sum_even, sum_odd = 0,0
+sum_even, sum_odd = 0, 0
 
 for num in list_of_numbers:
-    if num%2==0:
+    if num % 2 == 0:
         sum_even += num
     else:
         sum_odd += num
